3697a7b438a035c62ab94b587a624709d1abb741
arpansaha13 <arpansaha1300@gmail.com>
2025-09-20 21:25:11 +0530

    chore: setup redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
+import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
+import { store } from './store'
 import AppRoutes from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')
+
+if (!container) {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}
+
+const root = createRoot(container)
+
+root.render(
   <StrictMode>
-    <AppRoutes />
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
   </StrictMode>,
 )
