b0ea2c7b9bcffeba175514aacad71f52a90dc400
arpansaha13 <arpansaha1300@gmail.com>
2025-09-21 17:56:22 +0530

    refactor: modular components for tables

---
+++ src/components/Table.tsx
@@ -0,0 +1,73 @@
+import type { PropsWithChildren } from 'react'
+import { classNames } from '../utils/classNames'
+
+// Table
+type TableProps = PropsWithChildren<{
+  className?: string
+}>
+
+export function Table({ children, className }: TableProps) {
+  return (
+    <table className={classNames('w-full border-collapse', className)}>
+      {children}
+    </table>
+  )
+}
+
+// THead
+type THeadProps = PropsWithChildren<{
+  className?: string
+}>
+
+export function THead({ children, className }: THeadProps) {
+  return <thead className={classNames('', className)}>{children}</thead>
+}
+
+// TBody
+type TBodyProps = PropsWithChildren<{
+  className?: string
+}>
+
+export function TBody({ children, className }: TBodyProps) {
+  return (
+    <tbody
+      className={classNames(
+        'divide-dark/5 dark:divide-light/10 divide-y',
+        className,
+      )}
+    >
+      {children}
+    </tbody>
+  )
+}
+
+// TRow
+type TRowProps = PropsWithChildren<{
+  className?: string
+}>
+
+export function TRow({ children, className }: TRowProps) {
+  return <tr className={classNames('h-10', className)}>{children}</tr>
+}
+
+// TCell
+type TCellProps = PropsWithChildren<{
+  className?: string
+  isHeader?: boolean
+}>
+
+export function TCell({ children, className, isHeader = false }: TCellProps) {
+  const Component = isHeader ? 'th' : 'td'
+  return (
+    <Component
+      className={classNames(
+        'text-xs',
+        isHeader &&
+          'dark:text-light/40 text-dark/40 dark:border-light/10 border-b border-[#e5e7eb] text-left font-medium',
+        className,
+      )}
+    >
+      {children}
+    </Component>
+  )
+}
--- src/components/charts/TopSelling.tsx
+++ src/components/charts/TopSelling.tsx
@@ -1,37 +1,30 @@
 import Card from '../Card.tsx'
 import { Tableproducts } from './data.tsx'
+import { Table, THead, TBody, TRow, TCell } from '../Table.tsx'
 
 const TopSellingProductsTable = () => {
   return (
     <Card title="Top Selling Products">
-      <table className="w-full border-collapse">
-        <thead>
-          <tr className="h-10">
-            <th className="dark:text-light/40 text-dark/40 dark:border-light/10 border-b border-[#e5e7eb] text-left text-xs font-medium">
-              Name
-            </th>
-            <th className="dark:text-light/40 text-dark/40 dark:border-light/10 border-b border-[#e5e7eb] text-left text-xs font-medium">
-              Price
-            </th>
-            <th className="dark:text-light/40 text-dark/40 dark:border-light/10 border-b border-[#e5e7eb] text-left text-xs font-medium">
-              Quantity
-            </th>
-            <th className="dark:text-light/40 text-dark/40 dark:border-light/10 border-b border-[#e5e7eb] text-left text-xs font-medium">
-              Amount
-            </th>
-          </tr>
-        </thead>
-        <tbody>
+      <Table>
+        <THead>
+          <TRow>
+            <TCell isHeader>Name</TCell>
+            <TCell isHeader>Price</TCell>
+            <TCell isHeader>Quantity</TCell>
+            <TCell isHeader>Amount</TCell>
+          </TRow>
+        </THead>
+        <TBody>
           {Tableproducts.map(product => (
-            <tr key={product.name} className="h-10">
-              <td className="text-xs">{product.name}</td>
-              <td className="text-xs">{product.price}</td>
-              <td className="text-xs">{product.quantity}</td>
-              <td className="text-xs">{product.amount}</td>
-            </tr>
+            <TRow key={product.name}>
+              <TCell>{product.name}</TCell>
+              <TCell>{product.price}</TCell>
+              <TCell>{product.quantity}</TCell>
+              <TCell>{product.amount}</TCell>
+            </TRow>
           ))}
-        </tbody>
-      </table>
+        </TBody>
+      </Table>
     </Card>
   )
 }
